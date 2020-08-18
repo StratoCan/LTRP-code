98bc3acff2dd81a02c066cbf5aeaf21c333f278f
Tassilo Balbo <contact@filipsikora.com>
2020-08-18 15:25:25 +0100

    LTRP code template

--- LTRP_code/LTRP_code.ino
+++ LTRP_code/LTRP_code.ino
@@ -10,3 +10,37 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+//SEQUENCES
+
+void startUpSequence(){
+
+}
+void fallSequence(){
+
+}
+
+void groundSequence(){
+
+}
+
+//SEQUENCES END
+
+//ROUTINES AND SUBSEQUENCES
+
+void chuteDetach(){
+	//subsequence
+}
+
+void standUp(){
+	//stand up routine used to lift the LTRP
+}
+
+void hiberanate(){
+	//hibernate routine to conservate battery capacity
+}
+
+//ROUTINES AND SUBSEQUENCES END
+
+
