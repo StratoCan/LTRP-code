fc151b3cc035f36c9caaaa217ea178939bacabf4
JiriVala <jiri.vala11@gmail.com>
2020-08-24 04:26:23 +0200

    I'm havin' a seizure

--- csvParser/parser.js
+++ csvParser/parser.js
@@ -1,27 +1,52 @@
- var data;
+
+ var data;//idk premade variable
+
+ var actualData;
+ var datalog;
 	 
 	function handleFileSelect(evt) {
 		var file = evt.target.files[0];
 		console.log('changed!');
 		Papa.parse(file, {
-		  header: true,
+		  header: false,
 		  dynamicTyping: true,
 		  complete: function(results) {
-			data = results;
-			console.log(results);
+			console.log("FINISHED PARSING!", results.data);
+			//console.table(results.data);
+			actualData = results.data;
+
+			for(x = 0; x < actualData.length; x++){
+				actualData.forEach(stringToArray);
+			}
+			return datalog;
+
 		  }
 		});
 	}
-	 
-	/*function createTable() {
-		var array = results; 
-		document.getElementById("1").innerHTML = ""; //Clear.
-		
-		for (i = 0; i < array.length; i++) { 
-		document.getElementById("1").innerHTML += "<tr>";
-			for (k = 0; k < array[0].length; k++) {
-			  document.getElementById("1").innerHTML += "<td>" + array[i][k] + "</td>" ;
-			}
-			document.getElementById("1").innerHTML += "</tr>";
-		}
-	}*/
+
+	function stringToArray(value){
+		var splitted = value.split(",");
+		datalog = new DATA(splitted);
+	}
+
+	//DATA, ERRORS ->  ARRAY OF FUCKEN ARRAYS
+	//META -> OBJECT
+
+	function DATA(id, time, DHTtemp, DHThum, BMPtemp, BMPpress, BMPalt, BMEtemp, BMEpress, BMEalt, BMEhum, yaw, pitch, roll){
+		this.id = id;
+		this.time = time;
+		this.DHTtemp = DHTtemp;
+		this.DHThum = DHThum;
+		this.BMPtemp = BMPtemp;
+		this.BMPpress = BMPpress;
+		this.BMPalt = BMPalt;
+		this.BMEtemp = BMEtemp;
+		this.BMEpress = BMEpress;
+		this.BMEalt = BMEalt;
+		this.BMEhum = BMEhum;
+		this.yaw = yaw;
+		this.pitch = pitch;
+		this.roll = roll;
+	}
+
+	
